61f5030a56eac1f86df4d4df02807ec20897f515
QIU Quan <jackqq@gmail.com>
2016-05-17 15:46:03 +0800

    Add DSC resource xTextEdit.

---
+++ DSCResources/xTextEdit/xTextEdit.psm1
@@ -0,0 +1,85 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Path,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SetScript,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $TestScript
+    )
+
+    return @{
+    }
+}
+
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Path,
+
+        [System.String]
+        $Encoding = "utf8",
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SetScript,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $TestScript
+    )
+
+    $ss = Get-Content $Path -Encoding $Encoding
+
+    $script = [ScriptBlock]::Create($SetScript)
+
+    & $script
+
+    $ss | Out-File $Path -Encoding $Encoding
+}
+
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Path,
+
+        [System.String]
+        $Encoding = "utf8",
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SetScript,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $TestScript
+    )
+
+    $ss = Get-Content $Path -Encoding $Encoding
+
+    $script = [ScriptBlock]::Create($TestScript)
+
+    return & $script
+}
+
+
+Export-ModuleMember -Function *-TargetResource
